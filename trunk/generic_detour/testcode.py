1a52efc529ba3d5e02b54ba409e64a29fc0c0f65
CBWhiz <CBWhiz@gmail.com>
2008-06-22 05:31:30 +0000

    nice dump

--- trunk/generic_detour/testcode.py
+++ trunk/generic_detour/testcode.py
@@ -4,32 +4,33 @@ import detour
 
 print "test.py loaded"
 
+class register_list:
+	def __init__(self, registertuple, flags):
+		self.eax = registertuple[0]
+		self.ecx = registertuple[1]
+		self.edx = registertuple[2]
+		self.ebx = registertuple[3]
+		self.esp = registertuple[4]
+		self.ebp = registertuple[5]
+		self.esi = registertuple[6]
+		self.edi = registertuple[7]
+		self.flags = flags
+	def __str__(self):
+		return "(EAX: 0x%08x, ECX: 0x%08x, EDX: 0x%08x, EBX: 0x%08x, ESP: 0x%08x, EBP: 0x%08x, ESI: 0x%08x, EDI: 0x%08x, flags: 0x%08x)"%(self.eax, self.ecx, self.edx, self.ebx, self.esp, self.ebp, self.esi, self.edi, self.flags)
+
+
 def myCallback(*args, **kwargs):
-	detouraddr, registers, flags, caller = args[0], args[1], args[2], args[3]
+	detouraddr, r, flags, caller = args[0], args[1], args[2], args[3]
+	registers = register_list(r, flags)
 
 	x = """Detour Address: 0x%08x
-Registers: 
-	EAX: 0x%08x
-	ECX: 0x%08x
-	EDX: 0x%08x
-	EBX: 0x%08x
-	ESP: 0x%08x
-	EBP: 0x%08x
-	ESI: 0x%08x
-	EDI: 0x%08x
-Flags: 0x%08x
-Caller: 0x%08x"""%(
+Caller: 0x%08x
+Registers: %s
+"""%(
 	detouraddr, 
-	registers[0], 
-	registers[1], 
-	registers[2], 
-	registers[3], 
-	registers[4], 
-	registers[5], 
-	registers[6], 
-	registers[7], 
-	flags, 
-	caller)
+	caller,
+	registers, 
+)
         print x
 
 detour.callback = myCallback
