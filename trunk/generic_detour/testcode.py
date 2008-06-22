4220dc07aa2128f37d4d31e6df4a7adfdbeb3559
CBWhiz <CBWhiz@gmail.com>
2008-06-22 09:05:28 +0000

    Actual python interface, can get/set options form python, can return to original

--- trunk/generic_detour/testcode.py
+++ trunk/generic_detour/testcode.py
@@ -1,10 +1,17 @@
-import sys
-import ctypes
-import detour
+import gdetour
 
-print "test.py loaded"
+print "testcode.py loaded"
+
+##############################################################
+##############################################################
+##############################################################
+#	Everything in this section should be regarded as API
+##############################################################
+##############################################################
+##############################################################
 
 class register_list:
+	"""Helpful register_list class. Holds the 8 main registers and the flags register"""
 	def __init__(self, registertuple, flags):
 		self.eax = registertuple[0]
 		self.ecx = registertuple[1]
@@ -18,19 +25,121 @@ class register_list:
 	def __str__(self):
 		return "(EAX: 0x%08x, ECX: 0x%08x, EDX: 0x%08x, EBX: 0x%08x, ESP: 0x%08x, EBP: 0x%08x, ESI: 0x%08x, EDI: 0x%08x, flags: 0x%08x)"%(self.eax, self.ecx, self.edx, self.ebx, self.esp, self.ebp, self.esi, self.edi, self.flags)
 
+class pyDetourConfig:
+	"""Configuration of a gdetour that can be changed throughout the detour's lifetime"""
+	def __init__(self, addr):
+		self.address = addr
+		self.callback = None
 
-def myCallback(*args, **kwargs):
+class callback_obj:
+	"""This is the object passed to function that are registed as callbacks. It should be the only way to interact with gdetour"""
+
+	def __init__(self, address, registers, caller):
+		self.address = address	
+		self.registers = registers
+		self.caller = caller
+
+	@staticmethod
+	def read(address, length):
+		return gdetour.read(address, length)
+
+	@staticmethod
+	def write(address, length):
+		return gdetour.write(address, length)
+
+	def dump(self):
+		print "Dump for call to 0x%08x from 0x%08x:"%(self.address, self.caller)
+		print "\tRegisters:"
+		print "\t\tEAX: 0x%08x" % (self.registers.eax)
+		print "\t\tECX: 0x%08x" % (self.registers.ecx)
+		print "\t\tEDX: 0x%08x" % (self.registers.edx)
+		print "\t\tEBX: 0x%08x" % (self.registers.ebx)
+		print "\t\tESP: 0x%08x" % (self.registers.esp)
+		print "\t\tEBP: 0x%08x" % (self.registers.ebp)
+		print "\t\tESI: 0x%08x" % (self.registers.esi)
+		print "\t\tEDI: 0x%08x" % (self.registers.edi)
+		print "\t\tflags: 0x%08x" % (self.registers.flags)
+		for i in xrange(1, 8):
+			if i == 1:
+				t = ""
+			else:
+				t = "+%X"%((i-1)*4)
+			a = self.getArg(i)
+			b = self.getStringArg(i)
+			if b is None:
+				print "\t[ESP%s]: 0x%08x" % (t, a)
+			else:
+				print "\t[ESP%s]: 0x%08x ('%s')" % (t, a, b)
+
+	def getArg(self, attrNum):
+		"""1 based argument getter function"""
+		add = (attrNum - 1) * 4 #4 byte paramters
+		return gdetour.readDWORD(self.registers.esp+add)
+
+	def getStringArg(self, attrNum):
+		addr = self.getArg(attrNum)
+		return gdetour.readASCIIZ(addr)
+
+	def getConfiguration(self):
+		n = ["bytesToPop", "executeOriginal"]
+		return dict(zip(n, gdetour.getDetourSettings(self.address)))
+
+	def setConfiguration(self, settingsDict):
+		n = ["bytesToPop", "executeOriginal"]
+		p = []
+		for x in n:
+			p.append(settingsDict[x])
+		gdetour.setDetourSettings(self.address, p)
+
+	def changeConfiguration(self, settingname, settingvalue):
+		d = self.getConfiguration()
+		d[settingname] = settingvalue
+		self.setConfiguration(d)
+
+
+
+detour_list = {}
+class Detour:
+	def __init__(self, address, overwrite_len, bytes_to_pop, return_to_original, callback=None, type=0):
+		if address in detour_list:
+			raise Exception, "Detour already exists!"
+		gdetour.createDetour(address, overwrite_len, bytes_to_pop, type)
+		gdetour.setDetourSettings(address, (bytes_to_pop, return_to_original))
+		self.config = pyDetourConfig(address)
+		self.config.callback = callback
+		detour_list[address] = self
+
+	def removeDetour(self):
+		raise NotImplementedError
+
+	@classmethod
+	def do_callback(cls, address, registers, caller):
+		if address not in detour_list:
+			raise Exception, "Detour... doesn't exist...?"
+		if callable(detour_list[address].config.callback):
+			detour_list[address].config.callback(callback_obj(address, registers, caller))
+
+
+
+def main_callback(*args, **kwargs):
 	detouraddr, r, flags, caller = args[0], args[1], args[2], args[3]
 	registers = register_list(r, flags)
 
-	x = """Detour Address: 0x%08x
-Caller: 0x%08x
-Registers: %s
-"""%(
-	detouraddr, 
-	caller,
-	registers, 
-)
-        print x
+	Detour.do_callback(detouraddr, registers, caller)
 
-detour.callback = myCallback
+
+gdetour.callback = main_callback	
+
+
+##############################################################
+##############################################################
+##############################################################
+##############################################################
+
+def testcb(d):
+	d.dump()
+	d.changeConfiguration("executeOriginal", 1)
+	print "\n" + repr(d.getConfiguration())
+	
+
+x = Detour(0x00ef1000, 6, 4, False, testcb)
