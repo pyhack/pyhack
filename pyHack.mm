3b61e4e5888ab54f29cb3da6a2b85bd4bfd3b93d
CBWhiz <CBWhiz@gmail.com>
2009-06-18 04:47:38 +0000

    Add mindmap

---
+++ pyHack.mm
@@ -0,0 +1,47 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1245299009306" ID="Freemind_Link_959465176" MODIFIED="1245299106262" TEXT="launcher / Bones">
+<node CREATED="1245299025189" ID="_" MODIFIED="1245299115515" POSITION="right" TEXT="apps">
+<node CREATED="1245299191714" ID="Freemind_Link_60260809" MODIFIED="1245299193407" TEXT="reandomnumber">
+<node CREATED="1245299246341" ID="Freemind_Link_712177833" MODIFIED="1245299252805" TEXT="__init__">
+<node COLOR="#999999" CREATED="1245299253857" ID="Freemind_Link_1090928320" MODIFIED="1245299310117" TEXT="RandomNumber">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+<node COLOR="#006699" CREATED="1245299270988" ID="Freemind_Link_1729168924" MODIFIED="1245299320922" TEXT="lawnmower.py"/>
+<node COLOR="#006699" CREATED="1245299332242" ID="Freemind_Link_1774364516" MODIFIED="1245299345794" TEXT="wallhack.py"/>
+<node COLOR="#006699" CREATED="1245299336912" ID="Freemind_Link_310989012" MODIFIED="1245299346519" TEXT="vampire_jump.py"/>
+<node COLOR="#006699" CREATED="1245299359600" ID="Freemind_Link_1118609022" MODIFIED="1245299363809" TEXT="mlog_log.py"/>
+</node>
+<node CREATED="1245299276951" ID="Freemind_Link_1006574745" MODIFIED="1245299278623" TEXT="common">
+<node COLOR="#006699" CREATED="1245299279076" ID="Freemind_Link_661928276" MODIFIED="1245299377961" TEXT="winsock_log.py"/>
+<node COLOR="#006699" CREATED="1245299369572" ID="Freemind_Link_364966978" MODIFIED="1245299379974" TEXT="hack_check_patches.py"/>
+<node CREATED="1245299383610" ID="Freemind_Link_1042984523" MODIFIED="1245299386937" TEXT="unpackers">
+<node COLOR="#006699" CREATED="1245299387942" ID="Freemind_Link_1040594978" MODIFIED="1245299395496" TEXT="asprotect_1.23"/>
+</node>
+</node>
+</node>
+<node CREATED="1245299406503" ID="Freemind_Link_1849833989" MODIFIED="1245299409020" POSITION="right" TEXT="sig_finders">
+<node COLOR="#006699" CREATED="1245299409647" ID="Freemind_Link_1969624466" MODIFIED="1245299413304" TEXT="basic.py"/>
+<node COLOR="#006699" CREATED="1245299414497" ID="Freemind_Link_232880788" MODIFIED="1245299422773" TEXT="graph_check.py"/>
+</node>
+<node CREATED="1245299120813" ID="Freemind_Link_306499320" MODIFIED="1245299126117" POSITION="right" TEXT="pyHack">
+<node CREATED="1245299131862" ID="Freemind_Link_1284067237" MODIFIED="1245299134568" TEXT="detour_api">
+<node CREATED="1245299148009" ID="Freemind_Link_166964536" MODIFIED="1245299149662" TEXT="inside"/>
+<node CREATED="1245299150423" ID="Freemind_Link_1225732300" MODIFIED="1245299152008" TEXT="outside"/>
+</node>
+<node CREATED="1245299136010" ID="Freemind_Link_1796389282" MODIFIED="1245299137505" TEXT="util"/>
+</node>
+<node CREATED="1245299158309" ID="Freemind_Link_147997937" MODIFIED="1245299174752" POSITION="right" TEXT="pyDetour (DLL)"/>
+<node CREATED="1245300091369" ID="Freemind_Link_1298082260" MODIFIED="1245300094821" POSITION="right" TEXT="local_config">
+<node CREATED="1245300112356" ID="Freemind_Link_1747794201" MODIFIED="1245300118987" TEXT="target_config"/>
+<node CREATED="1245300095321" ID="Freemind_Link_164333349" MODIFIED="1245300099681" TEXT="target_defs"/>
+</node>
+<node CREATED="1245299565895" ID="Freemind_Link_1652526410" MODIFIED="1245299578384" POSITION="left" TEXT="Bones / Launch Workflow">
+<node CREATED="1245299579143" ID="Freemind_Link_518286775" MODIFIED="1245299603520" TEXT="1. Search recursively under apps for .py files"/>
+<node CREATED="1245299613351" ID="Freemind_Link_718777551" MODIFIED="1245299689206" TEXT="2. If you find a package (detonated by __init__), add it&apos;s &apos;target meta&apos; to list of known target concepts"/>
+<node CREATED="1245299693351" ID="Freemind_Link_20260844" MODIFIED="1245299786994" TEXT="3. Otherwise, import the python files and if they are a hack/plugin/whatever, show it on the right listbox as a toggleable selection"/>
+</node>
+</node>
+</map>
