d79f4a91bc4b1a659944ea51e5ad4e6e0ae3e27d
Dustin Lacewell <dlacewell@gmail.com>
2009-06-18 02:31:52 +0000

    Refactored out general code from randomNumber.py APP into a class. AP Plugins simply need to:
    1. Define a CommonPatch subclass.
    2. Create a tuple of tuples describing the patches to apply in the form of -
        (name, patch_instance, bApply)
    3. Instantiate APPlugin passing in the patches
    4. Tell the APPlugin instance to .go()

--- trunk/pyhack/apps/randomNumber.py
+++ trunk/pyhack/apps/randomNumber.py
@@ -1,81 +1,29 @@
-#----------------------------------------------------------------
-import os, pickle, sys
-conf = pickle.loads(os.environ['pyDetourConfig'])
-sys.path.append(os.path.dirname(conf['dll']))
-sys.path.append(os.path.dirname(conf['targetDef']['pycode']))
-#----------------------------------------------------------------
-import logging
-rootLog = logging.getLogger("")
-rootLog.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
-ch.setFormatter(formatter) #add formatter to handler
-rootLog.addHandler(ch) #add the handlers to logger
-del formatter
-del ch
-log = logging.getLogger(conf['targetName'])
-#----------------------------------------------------------------
-import pprint
-log.info("Configuration:\n" + pprint.pformat(conf))
-#----------------------------------------------------------------
-import ctypes
-kernel32 = ctypes.windll.kernel32
-
-def resumeThread(threadId):
-	THREAD_SUSPEND_RESUME = 0x0002
-	hThread = kernel32.OpenThread(THREAD_SUSPEND_RESUME, False, threadId)
-	kernel32.ResumeThread(hThread)
-	kernel32.CloseHandle(hThread)
-
-import detour_api
-pyVer = "%s.%s.%s %s"%(sys.version_info[0], sys.version_info[1], sys.version_info[2], sys.version_info[3])
-if detour_api.misc.pythonDebug:
-	log.info("Running under Python debug version %s"%(pyVer))
-else:
-	log.info("Running under Python release version%s"%(pyVer))
-
-
-##############################################################
-
-def returnTrue(d):
-	d.registers.eax = 1
-
-def returnFalse(d):
-	d.registers.eax = 0
-
-flag = False
-def returnTrueOnce(d):
-	if not globals()['flag']:
-		d.registers.eax = 1
-		globals()['flag'] = True
-	else:
-		d.registers.eax = 0
-
-sym = {}
-sym['isValid'] = 0x9A1030
-sym['getRandomNumber'] = 0x9A1000
-
-m = detour_api.memory
-
-p = detour_api.PatchManager()
-p.addCommonPatch("kernel32.IsDebuggerPresent").apply()
+from pyhack.util.app import APPlugin
+from pyhack import detour_api
 
 class NumberPatch(detour_api.CommonPatch):
 	"""NumberPatch detours the target's getRandomNumber() function to return 42"""
+	
+	offs = {
+		'isValid' : 0x00401030,
+		'getRandomNumber' : 0x00401000,
+	}
+
 	def __init__(self):
 		detour_api.CommonPatch.__init__(self)
-		d = detour_api.Detour(sym['getRandomNumber'], False, self.return42)
+		dtr = detour_api.Detour(
+			NumberPatch.offs['getRandomNumber'], 
+			False, self.return42
+		)
 		name = "IsDebuggerPresent? No."
-		self.detours.append(d)
+		self.detours.append(dtr)
 		
-	def return42(self, d):
-		print self
-		print d
-		d.registers.eax = 42
+	def return42(self, dtr):
+		dtr.registers.eax = 42
 
-p["number"] = NumberPatch()
-p["number"].apply()
+patches = (
+     # name,	patch,	apply
+	("number", NumberPatch(), True),
+)
 
-pprint.pprint(p)
-detour_api.misc.interact(globals(), locals())
+APPlugin(patches).go()
---
+++ trunk/pyhack/util/app.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import pickle
+import logging
+import pprint
+import ctypes
+kernel32 = ctypes.windll.kernel32
+
+from defines import ENVCONFVAR
+def _setup_target_environ():
+		pickled_conf = os.environ[ENVCONFVAR]
+		config = pickle.loads(pickled_conf)
+		# Add required paths to environment
+		dll_path = os.path.dirname(config['dll'])
+		target_conf = config['targetDef']
+		script_path = os.path.dirname(target_conf['pycode'])
+		sys.path.append(dll_path)
+		sys.path.append(script_path)
+_setup_target_environ()
+
+from pyhack import detour_api
+
+
+class APPlugin(object):
+	def __init__(self, patches):
+		self._setup_target_environ(ENVCONFVAR)
+		self._setup_logging()
+		
+		self.memory = detour_api.memory
+		self.patches = detour_api.PatchManager()
+		# Hide from debugger
+		self.patches.addCommonPatch("kernel32.IsDebuggerPresent").apply()
+		
+		pyVersion = "%s.%s.%s %s" % tuple(sys.version_info[:4])
+		if detour_api.misc.pythonDebug:
+			self.log.info("Running under Python DEBUG version %s" % pyVersion)
+		else:
+			self.log.info("Running under Python RELEASE version %s" % pyVersion)
+		
+		for name, patch, apply in patches:
+			self.patches[name] = patch
+			if apply:
+				self.patches[name].apply()
+				pprint.pprint(self.patches[name])
+				
+	def go(self):
+		detour_api.misc.interact(globals(), locals())
+		
+	def _setup_target_environ(self, osvar):
+		pickled_conf = os.environ[osvar]
+		self.config = pickle.loads(pickled_conf)
+		# Add required paths to environment
+		dll_path = os.path.dirname(self.config['dll'])
+		target_conf = self.config['targetDef']
+		script_path = os.path.dirname(target_conf['pycode'])
+		sys.path.append(dll_path)
+		sys.path.append(script_path)
+		
+	def _setup_logging(self):
+		self.rootlog = logging.getLogger("")
+		self.rootlog.setLevel(logging.DEBUG)
+		handler = logging.StreamHandler()
+		handler.setLevel(logging.DEBUG)
+		format = logging.Formatter(
+		    "%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+		handler.setFormatter(format)
+		del format
+		del handler
+		self.log = logging.getLogger(self.config['targetName'])
+		
+	def resumeThread(self, thread_id):
+		THREAD_SUSPEND_RESUME = 0x0002
+		handle = kernel32.OpenThread(THREAD_SUSPEND_RESUME, False, thread_id)
+		kernel32.ResumeThread(handle)
+		kernel32.CloseHandle(handle)
+		
+	def returnTrue(self, detour):
+		detour.registers.eax = 1
+		
+	def returnFalse(self, detour):
+		detour.registers.eax = 0
+			
+		
