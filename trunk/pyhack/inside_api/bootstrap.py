d584fe8affad24a789c651dc51709da7d1bf2486
CBWhiz <CBWhiz@gmail.com>
2009-07-13 07:38:38 +0000

    add logging to inside code

--- trunk/pyhack/inside_api/bootstrap.py
+++ trunk/pyhack/inside_api/bootstrap.py
@@ -1,6 +1,8 @@
 """This file is the main starting point for the python interpreter when in the target's process.
 It needs to setup the paths, and then import the real target script"""
 
+import logging
+
 import os, pickle, sys, imp
 
 sys.path.append(os.environ['pyHack_ParentPath']) #This lets us import pyhack
@@ -15,8 +17,47 @@ targetDef = conf['targetDef']
 import pydetour
 import pyhack
 import pyhack.apps #Prevents 'parent module not loaded' errors
+import pyhack.inside_api
+__name__ = "pyhack.inside_api.bootstrap"
 
-modname = os.path.splitext(os.path.basename(targetDef.pycode))
+class DiscardFilter(logging.Filter):
+    def filter(self, record):
+        if logging.Filter.filter(self, record) == 1:
+            return 0
+        return 1
 
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+log_console_handler = logging.StreamHandler()
+log_console_handler.setLevel(logging.DEBUG)
+log_console_formatter = logging.Formatter("%(funcName)s: %(message)s")
+log_console_handler.setFormatter(log_console_formatter)
+log_console_handler.addFilter(DiscardFilter('pyhack.inside_api'))
+log.addHandler(log_console_handler)
+
+
+
+log_fh = logging.FileHandler("pyhack_last_run.txt", "w")
+log_ff = logging.Formatter("%(asctime)-15s %(levelname)-8s %(name)s.%(funcName)s: %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
+log_fh.setFormatter(log_ff)
+log.addHandler(log_fh)
+
+
+
+log = logging.getLogger(__name__)
+
+modname = os.path.splitext(os.path.basename(targetDef.pycode))[0]
+
+log.info("Importing pyhack.apps.%s from inside target"%(modname, ))
 with open(targetDef.pycode, "rb") as f:
-    mod = imp.load_source("pyhack.apps." + modname[0], targetDef.pycode, f)
+    try:
+        mod = imp.load_source("pyhack.apps." + modname, targetDef.pycode, f)
+    except Exception:
+        log.exception("Encountered exception in processing plugin")
+        import pdb
+        import sys
+        import traceback
+        traceback.print_exc()
+        print
+        pdb.post_mortem(sys.exc_info()[2])
