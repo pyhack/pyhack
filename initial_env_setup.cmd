8d5d129112ce35ab016b24938c13cf5d7a64f970
CBWhiz <CBWhiz@gmail.com>
2011-06-13 00:14:54 -0400

    Update initial_env script to Python 2.7.2

--- initial_env_setup.cmd
+++ initial_env_setup.cmd
@@ -1,9 +1,9 @@
 @echo off
 set PWD=%CD%
 set HERE=%~pd0
-set PYTHON_TAR=Python-2.7.1.tar.bz2
-set PYTHON_TAR_XFOLDER=Python-2.7.1
-set PYTHON_URL=http://www.python.org/ftp/python/2.7.1/Python-2.7.1.tar.bz2
+set PYTHON_TAR=Python-2.7.2.tar.bz2
+set PYTHON_TAR_XFOLDER=Python-2.7.2
+set PYTHON_URL=http://www.python.org/ftp/python/2.7.2/Python-2.7.2.tar.bz2
 
 cd /d %HERE%
 
@@ -17,15 +17,31 @@ tar -xf %PYTHON_TAR%
 mv %PYTHON_TAR_XFOLDER% python
 
 :set_vs9_env
-set VS9=%ProgramFiles(x86)%\Microsoft Visual Studio 9.0
+setlocal
+set pf=
+if exist "%ProgramFiles(x86)%" set pf=%ProgramFiles(x86)%
+if not exist "%pf%" set pf=%ProgramFiles%
+set VS9=%pf%\Microsoft Visual Studio 9.0
+endlocal & set VS9=%VS9%
+if not exist "%VS9%\VC\vcvarsall.bat" goto novs9
 call "%VS9%\VC\vcvarsall.bat"
-set VS9=%ProgramFiles%\Microsoft Visual Studio 9.0
-call "%VS9%\VC\vcvarsall.bat"
+goto build_python
+
+:novs9
+echo Visual Studio 9.0 (2008) was not found.
+echo.
 
 :build_python
 cd python\PCBuild
-call build.bat
-call build.bat -d
+if not exist python.exe call build.bat
+if not exist python_d.exe call build.bat -d
 
 :end
 cd /d %PWD%
+echo.
+echo.
+if exist python\PCBuild\python.exe echo python.exe sucessfully created
+if not exist python\PCBuild\python.exe echo python.exe was not sucessfully created
+if exist python\PCBuild\python_d.exe echo python_d.exe sucessfully created
+if not exist python\PCBuild\python_d.exe echo python_d.exe was not sucessfully created
+echo.
